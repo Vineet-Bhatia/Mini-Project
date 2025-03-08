877a753d5f67e9d821a1f99f2a0a32cd24f902a0
Vineet-Bhatia <vineetbhatia11@gmail.com>
2025-03-08 17:44:22 +0530

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { ContactComponent } from './contact/contact.component';
-import { Component } from '@angular/core';
 
-export const routes: Routes = [
-  {path:'contact',component:ContactComponent}
-];
+export const routes: Routes = [];
